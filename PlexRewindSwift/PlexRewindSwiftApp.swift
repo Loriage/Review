61730eb23dcebc1ac777789226fdacff17b39a78
Loriage <loriage09@gmail.com>
2025-08-04 17:24:39 +0200

    Initial Commit

---
+++ PlexRewindSwift/PlexRewindSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlexRewindSwiftApp.swift
+//  PlexRewindSwift
+//
+//  Created by Bruno DURAND on 04/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct PlexRewindSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
