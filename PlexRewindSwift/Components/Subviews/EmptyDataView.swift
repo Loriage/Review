cdea05728516b4c9deceee4aab0c8ecb7509e037
Loriage <loriage09@gmail.com>
2025-08-12 20:05:55 +0200

    removing old StatsView & rafactoring

---
+++ PlexRewindSwift/Components/Subviews/EmptyDataView.swift
@@ -0,0 +1,23 @@
+import SwiftUI
+
+struct EmptyDataView: View {
+    let systemImageName: String
+    let title: String
+    let message: String
+
+    var body: some View {
+        VStack(alignment: .center, spacing: 10) {
+            Image(systemName: systemImageName)
+                .font(.system(size: 40))
+                .foregroundColor(.secondary)
+            Text(title)
+                .font(.title3.bold())
+            Text(message)
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                .multilineTextAlignment(.center)
+        }
+        .frame(maxWidth: .infinity)
+        .padding(.vertical, 50)
+    }
+}
