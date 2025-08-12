9b6e14421c500d1cf5ab0b29a2daae608eab448d
Loriage <loriage09@gmail.com>
2025-08-12 21:06:59 +0200

    refactoring main view

---
+++ PlexRewindSwift/Views/AuthenticatedContentView.swift
@@ -0,0 +1,33 @@
+import SwiftUI
+
+struct AuthenticatedContentView: View {
+    @ObservedObject var authManager: PlexAuthManager
+
+    @StateObject private var serverViewModel: ServerViewModel
+    @StateObject private var activityViewModel: ActivityViewModel
+    @StateObject private var statsViewModel: StatsViewModel
+    @StateObject private var topStatsViewModel: TopStatsViewModel
+
+    init(authManager: PlexAuthManager) {
+        self.authManager = authManager
+        
+        let serverVM = ServerViewModel(authManager: authManager)
+        
+        _serverViewModel = StateObject(wrappedValue: serverVM)
+        _activityViewModel = StateObject(wrappedValue: ActivityViewModel(serverViewModel: serverVM))
+        _statsViewModel = StateObject(wrappedValue: StatsViewModel(serverViewModel: serverVM))
+        _topStatsViewModel = StateObject(wrappedValue: TopStatsViewModel(serverViewModel: serverVM, authManager: authManager))
+    }
+
+    var body: some View {
+        MainTabView()
+            .environmentObject(authManager)
+            .environmentObject(serverViewModel)
+            .environmentObject(activityViewModel)
+            .environmentObject(statsViewModel)
+            .environmentObject(topStatsViewModel)
+            .task {
+                await serverViewModel.loadServers()
+            }
+    }
+}
--- PlexRewindSwift/Views/MainView.swift
+++ PlexRewindSwift/Views/MainView.swift
@@ -4,50 +4,16 @@ struct MainView: View {
     @StateObject private var authManager = PlexAuthManager()
 
     var body: some View {
-        if authManager.isAuthenticated {
-            AuthenticatedContentView(authManager: authManager)
-        } else if authManager.pin != nil {
-            LoginPinView()
-                .environmentObject(authManager)
-        } else {
-            OnboardingView()
-                .environmentObject(authManager)
-        }
-    }
-}
-
-struct AuthenticatedContentView: View {
-    @ObservedObject var authManager: PlexAuthManager
-    @StateObject private var serverViewModel: ServerViewModel
-    @StateObject private var activityViewModel: ActivityViewModel
-    @StateObject private var statsViewModel: StatsViewModel
-    @StateObject private var topStatsViewModel: TopStatsViewModel
-
-    init(authManager: PlexAuthManager) {
-        self.authManager = authManager
-        
-        let libraryService = PlexLibraryService()
-        let userService = PlexUserService()
-        let activityService = PlexActivityService()
-        let metadataService = PlexMetadataService()
-
-        let serverVM = ServerViewModel(authManager: authManager, libraryService: libraryService, userService: userService)
-        _serverViewModel = StateObject(wrappedValue: serverVM)
-        
-        _activityViewModel = StateObject(wrappedValue: ActivityViewModel(serverViewModel: serverVM, activityService: activityService))
-        _statsViewModel = StateObject(wrappedValue: StatsViewModel(serverViewModel: serverVM, activityService: activityService, metadataService: metadataService))
-        _topStatsViewModel = StateObject(wrappedValue: TopStatsViewModel(serverViewModel: serverVM, authManager: authManager, activityService: activityService, metadataService: metadataService))
-    }
-
-    var body: some View {
-        MainTabView()
-            .environmentObject(authManager)
-            .environmentObject(serverViewModel)
-            .environmentObject(activityViewModel)
-            .environmentObject(statsViewModel)
-            .environmentObject(topStatsViewModel)
-            .task {
-                await serverViewModel.loadServers()
+        Group {
+            if authManager.isAuthenticated {
+                AuthenticatedContentView(authManager: authManager)
+            } else if authManager.pin != nil {
+                LoginPinView()
+                    .environmentObject(authManager)
+            } else {
+                OnboardingView()
+                    .environmentObject(authManager)
             }
+        }
     }
 }
