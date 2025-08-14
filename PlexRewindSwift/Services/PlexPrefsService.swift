6e38b884961ece039288a43e9abb993377a01bc1
Loriage <loriage09@gmail.com>
2025-08-14 21:00:58 +0200

    remove trailing spaces

--- PlexRewindSwift/Services/PlexPrefsService.swift
+++ PlexRewindSwift/Services/PlexPrefsService.swift
@@ -32,12 +32,12 @@ class PlexPrefsService {
             URLQueryItem(name: key, value: value),
             URLQueryItem(name: "X-Plex-Token", value: token)
         ]
-        
+
         guard let url = components.url else { throw PlexError.invalidURL }
-        
+
         var request = URLRequest(url: url)
         request.httpMethod = "PUT"
-        
+
         let (_, response) = try await URLSession.shared.data(for: request)
         
         guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
